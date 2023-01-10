c63c3922a8862a938acf89ff34163291af032795
rg81893 <rguillorit@gmail.com>
2023-01-10 14:37:45 +0100

    point 8

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
@@ -7,15 +8,21 @@ using static UnityEngine.GraphicsBuffer;
 
 public class Spawner : MonoBehaviour
 {
+    [SerializeField] int maxObjectsToSpawn;
+    [SerializeField] List<GameObject> gameObjects = new List<GameObject>();
     [SerializeField] GameObject objectToSpawn;
     [SerializeField] float spawnSpeed;
     [SerializeField] float distanceSpawn;
     [SerializeField] float cubeSpeed;
     float time = 0f;
+    List<GameObject> inactiveGameObjectsList = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < maxObjectsToSpawn; i++)
+        {
+            SpawnRandomObject();
+        }
     }
 
     // Update is called once per frame
@@ -23,48 +30,63 @@ public class Spawner : MonoBehaviour
     {
         if (spawnSpeed < 0f) return;
         time += Time.deltaTime;
-        //perimetre.transform.localScale = new Vector3(rayonPerimetre, 0.001f, rayonPerimetre);
         if (time >= 1 / spawnSpeed)
         {
-            float x = Random.Range(0.05f, 0.95f);
-            float y = Random.Range(0.05f, 0.95f);
-            Vector3 pos = new Vector3(x, y, 10.0f);
-            pos = Camera.main.ViewportToWorldPoint(pos);
-
-            //Vector3 worldPosition = ScreenPositionIntoWorld(
-            //  // example screen center:
-            //  new Vector2(x, y),
-            //  // distance into the world from the screen:
-            //  distanceSpawn
-            //);
-            //Vector3 localPosition = Random.insideUnitSphere * rayonPerimetre;
-            Vector3 objectPosition = transform.TransformPoint(pos);
-            SpawnRandomObject(objectPosition);
+            //SpawnRandomObject();
+            //CubeActivate();
+            if (inactiveGameObjectsList.Count > 0)
+            {
+                inactiveGameObjectsList[0].SetActive(true);
+                inactiveGameObjectsList.RemoveAt(0);
+            }
             time = 0f;
         }
 
-        if(Input.GetMouseButtonDown(0)) 
+        if (Input.GetMouseButtonDown(0)) 
         {
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out hit))
             {
                 GameObject target = hit.transform.gameObject;
-                Destroy(target);
+                //Destroy(target);
+                for (int i = 0; i < maxObjectsToSpawn; i++)
+                {
+                    if (gameObjects[i] == target)
+                    {
+                        target.SetActive(false);
+                        inactiveGameObjectsList.Add(target);
+                    }
+                }
             }
         }
-         
     }
-    public static Vector3 ScreenPositionIntoWorld(Vector2 screenPosition, float distance)
+
+    void CubeActivate()
     {
-        Ray ray = Camera.main.ScreenPointToRay(screenPosition);
-        return (ray.direction.normalized * distance);
+        var inactive = gameObjects.Where(g => g.activeInHierarchy == false).FirstOrDefault();
+        if(inactive != null)
+            inactive.gameObject.SetActive(true);
     }
-    void SpawnRandomObject(Vector3 objectPosition)
+
+    void SetupObject(GameObject obj)
     {
+        obj.GetComponent<Renderer>().material.color = Random.ColorHSV();
+        obj.GetComponent<Rigidbody>().AddForce(-transform.forward * cubeSpeed, ForceMode.Impulse);
+        // ajouter un tag à l'objet
+    }
+
+    void SpawnRandomObject()
+    {
+        float x = Random.Range(0.05f, 0.95f);
+        float y = Random.Range(0.05f, 0.95f);
+        Vector3 pos = new Vector3(x, y, 10.0f);
+        pos = Camera.main.ViewportToWorldPoint(pos);
+        Vector3 objectPosition = transform.TransformPoint(pos);
         GameObject objectNew = Instantiate(objectToSpawn, new Vector3(objectPosition.x, objectPosition.y, distanceSpawn), Quaternion.identity) as GameObject;
         objectNew.GetComponent<Renderer>().material.color = Random.ColorHSV();
-        //objectNew.GetComponent<Rigidbody>().velocity = cubeSpeed * GetComponent<Rigidbody>().velocity.normalized;
         objectNew.GetComponent<Rigidbody>().AddForce(-transform.forward * cubeSpeed, ForceMode.Impulse);
+        SetupObject(objectNew);
+        gameObjects.Add(objectNew);
     }
 }
